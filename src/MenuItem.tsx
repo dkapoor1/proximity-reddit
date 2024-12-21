2669b97b08279d3d28b3ae2d76be0349ca60b940
Dylan Kapoor <dylankapoor@gmail.com>
2024-12-21 08:18:39 -0500

    remove pointless post creation

--- src/MenuItem.tsx
+++ src/MenuItem.tsx
@@ -5,18 +5,18 @@ Devvit.addMenuItem({
     label: 'Proximity Game',
     onPress: async (_, context) => {
         const { reddit, ui } = context;
-        const currentSubreddit = await reddit.getCurrentSubreddit();
-        await reddit.submitPost({
-        title: 'Proximity #1',
-        subredditName: currentSubreddit.name,
-        preview: (
-            <vstack padding="medium" cornerRadius="medium">
-            <text style="heading" size="medium">
-                Loading Proximity, a global word guessing game…
-            </text>
-            </vstack>
-        ),
-        });
-        ui.showToast(`Created custom post in r/${currentSubreddit.name}!`);
+        // const currentSubreddit = await reddit.getCurrentSubreddit();
+        // await reddit.submitPost({
+        // title: 'Proximity #1',
+        // subredditName: currentSubreddit.name,
+        // preview: (
+        //     <vstack padding="medium" cornerRadius="medium">
+        //     <text style="heading" size="medium">
+        //         Loading Proximity, a global word guessing game…
+        //     </text>
+        //     </vstack>
+        // ),
+        // });
+        ui.showToast(`Sort subreddit by new to see the latest game!`);
     },
 });
